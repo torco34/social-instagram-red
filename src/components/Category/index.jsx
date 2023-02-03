7990aaf36a90626f3d9b758691416b998d6edb0b
Torcoroma <torcoromaarias@gmail.com>
2023-02-03 15:58:00 -0500

    add stynd

---
+++ src/components/Category/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const IMAGEN = "https://i.imgur.com/dJa0Hpl.jpg";
+import { Ancort, Image } from "./styles";
+export const Category = ({ cover = IMAGEN, path, emoji: emoji = "?" }) => {
+  return (
+    <div>
+      <Ancort href={path}>
+        <Image src={IMAGEN} />
+        {emoji}
+      </Ancort>
+    </div>
+  );
+};
---
+++ src/components/ListOfCategories/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+const List = styled.ul`
+  background-color: red;
+`;
+const Item = styled.li`
+  color: blue;
+`;
+export { List, Item };
