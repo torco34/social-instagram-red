db8b420e5404d58d280d0244617cdf670da7ea2e
Torcoroma <torcoromaarias@gmail.com>
2023-02-04 12:57:45 -0500

    Add: style photoCar

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-
+import { ImgWrapper, Img, Button } from "./styles";
+const IMG =
+  "https://images.unsplash.com/photo-1518001589401-1743b61d1def?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
 export const PhotoCard = ({ id, like = 0, src }) => {
   return (
     <>
       <article>
         <a href={`/detail/${id}`}>
-          <div>
-            <img src={src} />
-          </div>
+          <ImgWrapper>
+            <Img src={IMG} />
+          </ImgWrapper>
         </a>
-        <button>{like} likes!</button>
+        <Button>{like} likes!</Button>
       </article>
     </>
   );
