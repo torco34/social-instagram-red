1d08664b66daf3328a3731fe8f551d62b3ae4fd0
Torcoroma <torcoromaarias@gmail.com>
2023-02-13 23:12:36 -0500

    icono lik listo

--- src/components/ListOfPhotoCard/index.jsx
+++ src/components/ListOfPhotoCard/index.jsx
@@ -6,7 +6,7 @@ export const ListOfPhotoCard = () => {
     <div>
       {" "}
       <ul>
-        {[0, 1, 2, 3].map((id) => (
+        {[0, 1, 2, 3, 4, 5, 6, 7].map((id) => (
           <PhotoCard key={id} />
         ))}
       </ul>
--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,23 +1,50 @@
-import React from "react";
-import { ImgWrapper, Img, Button } from "./styles";
-import { AiTwotoneHeart } from "react-icons/ai";
+import React, { useEffect, useRef, useState } from "react";
+import { Article, ImgWrapper, Img, Button } from "./styles";
+
+import { BsSuitHeart, BsSuitHeartFill } from "react-icons/bs";
 const IMG =
   "https://images.unsplash.com/photo-1518001589401-1743b61d1def?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+
 export const PhotoCard = ({ id, like = 0, src }) => {
+  const [show, setShow] = useState(false);
+  const element = useRef(null);
+  const [liked, setLinkd] = useState(false);
+
+  useEffect(
+    function () {
+      console.log(element.current);
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0];
+        if (isIntersecting) {
+          console.log("si,");
+          setShow(true);
+          observer.disconnect();
+        }
+      });
+      observer.observe(element.current);
+    },
+    [element]
+  );
+  const Icon = liked ? BsSuitHeartFill : BsSuitHeart;
   return (
     <>
-      <article>
-        <a href={`/detail/${id}`}>
-          <ImgWrapper>
-            <Img src={IMG} />
-          </ImgWrapper>
-        </a>
-        <Button>
+      <Article ref={element}>
+        {show && (
+          <>
+            <a href={`/detail/${id}`}>
+              <ImgWrapper>
+                <Img src={IMG} />
+              </ImgWrapper>
+            </a>
+          </>
+        )}
+
+        <Button onClick={() => setLinkd(!liked)}>
           {" "}
-          <AiTwotoneHeart size="30px" />
+          <Icon size="30px" />
           {like} likes!
         </Button>
-      </article>
+      </Article>
     </>
   );
 };
